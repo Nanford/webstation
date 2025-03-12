c0f0054bc32d8e2f2a4c208e04801a172e3c2221
Nanford <843395729@qq.com>
2025-03-12 18:04:59 +0800

    修改配置信息

--- app/config.py
+++ app/config.py
@@ -18,8 +18,8 @@ class Config:
     MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.gmail.com'
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'nixk.nf.lin@gmail.com'
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'lin7886392'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'myemail@gmail.com'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'password'
     MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER') or 'your-email@gmail.com'
     
     # 爬虫配置
